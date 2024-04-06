d65529061ecaaf0da6b26979a95ffabe567e04b3
iTheWUULN <96634321+iTheWUULN@users.noreply.github.com>
2024-04-06 21:16:13 +0300

    Add files via upload

---
+++ source/Configurations/Client_Settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+    botToken: "MTIyNjIxNzE1ODE1NDEyOTYwOA.GOKcxz.AbV_xf4JyRi9G3q6owTOjTrWo2heIYhL_PSti4",
+    clientOwner: ["795204246046769182"],
+    prefix: ["."],
+
+    voiceChannel: "1226217799761002496",
+
+    clientPresences: ["Zalorant"],
+    clientActivity: "Zalorant",
+    activityType: "STREAMING"
+ }
